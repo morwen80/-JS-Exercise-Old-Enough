77a7d33d20d4f8fe0dd69046f7d71a59dd5de175
Valeria <ribesnero80@gmail.com>
2018-10-03 21:30:25 +0100

    Create Algorythms.js

---
+++ algorithms.js
@@ -0,0 +1,11 @@
+#Create an algorithm to find whether a string contains only unique characters (means, no double)
+
+function unique(str) {
+    for (i = 0; i < str.length; i++) {
+     if(str.indexOf(str[i]) == str.lastIndexOf(str[i])) {
+       return true
+     } else {
+       return false
+     }
+    }
+}
